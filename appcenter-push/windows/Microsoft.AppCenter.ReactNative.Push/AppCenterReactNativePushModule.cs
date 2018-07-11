df2702ddc8b276e856ebe1fe35430d0d798e8a2b
Eric Rozell <erozell@outlook.com>
2018-07-10 21:33:03 -0400

    Adds UWP support for appcenter-push, except push notification launch support

---
+++ appcenter-push/windows/Microsoft.AppCenter.ReactNative.Push/AppCenterReactNativePushModule.cs
@@ -0,0 +1,51 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Microsoft.AppCenter.ReactNative.Shared;
+using ReactNative.Bridge;
+using ReactNative.Modules.Core;
+
+namespace Microsoft.AppCenter.ReactNative.Push
+{
+    public class AppCenterReactNativePushModule : ReactContextNativeModuleBase
+    {
+        private const string ErrorNotSupported = "E_NOTSUPPORTED";
+        private const string OnPushNotificationReceivedEvent = "AppCenterPushNotificationReceived";
+
+        public AppCenterReactNativePushModule(ReactContext reactContext, string appSecret)
+            : base(reactContext)
+        {
+            AppCenterReactNativeShared.ConfigureAppCenter(appSecret);
+            Microsoft.AppCenter.Push.Push.PushNotificationReceived += OnPushNotificationReceived;
+        }
+
+        public override string Name => "AppCenterReactNativePush";
+
+        [ReactMethod]
+        public async void setEnabled(bool enabled, IPromise promise)
+        {
+            await Microsoft.AppCenter.Push.Push.SetEnabledAsync(enabled);
+            promise.Resolve(null);
+        }
+
+        [ReactMethod]
+        public async void isEnabled(IPromise promise)
+        {
+            var enabled = await Microsoft.AppCenter.Push.Push.IsEnabledAsync();
+            promise.Resolve(enabled);
+        }
+
+        [ReactMethod]
+        public void sendAndClearInitialNotification(IPromise promise)
+        {
+            promise.Reject(ErrorNotSupported, "The lastSessionCrashReport API is not supported in UWP.");
+        }
+
+        private void OnPushNotificationReceived(object sender, Microsoft.AppCenter.Push.PushNotificationReceivedEventArgs e)
+        {
+            // TODO: add initial notification support using Push.CheckLaunchedFromNotification
+            Context.GetJavaScriptModule<RCTDeviceEventEmitter>()
+                .emit(OnPushNotificationReceivedEvent, AppCenterReactNativePushUtil.ConvertPushNotificationToJObject(e));
+        }
+    }
+}
---
+++ appcenter-push/windows/Microsoft.AppCenter.ReactNative.Push/AppCenterReactNativePushUtil.cs
@@ -0,0 +1,38 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Microsoft.AppCenter.Push;
+using Newtonsoft.Json.Linq;
+
+namespace Microsoft.AppCenter.ReactNative.Push
+{
+    static class AppCenterReactNativePushUtil
+    {
+        public static JObject ConvertPushNotificationToJObject(PushNotificationReceivedEventArgs e)
+        {
+            if (e == null)
+            {
+                return new JObject();
+            }
+
+            var result = new JObject
+            {
+                { "title", e.Title },
+                { "message", e.Message },
+            };
+
+            if (e.CustomData.Count > 0)
+            {
+                var customProperties = new JObject();
+                foreach (var pair in e.CustomData)
+                {
+                    customProperties.Add(pair.Key, pair.Value);
+                }
+
+                result.Add("customProperties", customProperties);
+            }
+
+            return result;
+        }
+    }
+}
